95823041fb29d293058fd5822d269bf8b3e605fa
CarterWilliamson08 <carterwilliamson8@gmail.com>
2025-10-30 15:49:16 -0400

    Finished script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ console.log("script.js loaded");
 
 let endpoint = "https://api.giphy.com/v1/gifs/search?api_key=xYnMu6IrefWYgOFIVpsSMpAfAROyFFAo&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
 
+const gifContainer = document.querySelector("#gif-container");
+const gifButton = document.querySelector("#fetch-gif-btn");
+
 //Retrieve dog gifs
 function getDogGif() {
     fetch(endpoint)
@@ -9,11 +12,19 @@ function getDogGif() {
     .then(data => {
         const gifs = data.data.map(g => g.images.original.url);
         console.log(gifs);
+
+        gifContainer.innerHTML = "";
+
+        for(const url of gifs) {
+            gifContainer.innerHTML += `<img src="${url}" class="col-3 mb-3 img-fluid" alt="Dog GIF">`;
+        }
     })
     .catch(error => {
         console.error("Error fetching dog gif:", error);
     });
 }
+
+
 
 
 //Button event listener
